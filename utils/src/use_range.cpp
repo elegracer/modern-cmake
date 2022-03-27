3632d2c035faa22cb01a5c064a9ff1a6e6c65846
elegracer <elegracer@gmail.com>
2022-03-27 11:23:21 +0800

    Add range-v3

---
+++ utils/src/use_range.cpp
@@ -0,0 +1,17 @@
+#include "core/core.h"
+
+#include "spdlog/fmt/fmt.h"
+#include "spdlog/fmt/bundled/ranges.h"
+
+#include "range/v3/view/sliding.hpp"
+
+int main(int argc, const char *argv[]) {
+
+
+    auto v = std::vector {1, 2, 3, 4, 5};
+    for (auto &&t : v | ranges::views::sliding(3)) {
+        fmt::print("{}\n", fmt::join(t.begin(), t.end(), ","));
+    }
+
+    return 0;
+}
